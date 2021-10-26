a79b184b717b6e62551b2c00921e9dd64e5125c8
Henry Garner <henry@henrygarner.com>
2021-10-26 14:16:56 +0100

    Seed random behaviour & mark rather than remove

--- src/witan/cic/duration_scenario/core.clj
+++ src/witan/cic/duration_scenario/core.clj
@@ -1,11 +1,13 @@
 (ns witan.cic.duration-scenario.core
   (:require [witan.cic.duration-scenario.io.read :as read]
             [witan.cic.duration-scenario.io.write :as write]
-            [witan.cic.duration-scenario.time :as time]))
+            [witan.cic.duration-scenario.time :as time]
+            [witan.cic.duration-scenario.random :as r]))
 
-(defn apply-cap
-  [{:keys [episodes beginning end duration snapshot-date provenance] :as period} target-placement duration-cap-days]
-  (let [episodes (-> (reduce (fn [[acc episodes] [{:keys [placement] :as episode} {:keys [offset]}]]
+(defn apply-stochastic-rule-to-period
+  [{:keys [episodes beginning end duration snapshot-date provenance] :as period} target-placement duration-cap-days probability-cap-applies random-seed]
+  (let [cap-triggered? (<= (r/rand-long random-seed) probability-cap-applies)
+        episodes (-> (reduce (fn [[acc episodes] [{:keys [placement] :as episode} {:keys [offset]}]]
                                (let [duration (if offset
                                                 (- offset acc)
                                                 (- duration (:offset episode)))]
@@ -23,36 +25,39 @@
                                           [0 []]
                                           episodes)]
     (if (not= total-duration duration)
-      (-> period
-          (assoc :episodes episodes)
-          (assoc :duration total-duration)
-          (assoc :end (time/days-after beginning total-duration)))
-      period)))
-
-(defn apply-stochastic-rule-to-period
-  [period placement duration-cap-days probability-cap-applies]
-  (let [cap-triggered? (<= (rand) probability-cap-applies)]
-    (if cap-triggered? 
-      (apply-cap period placement duration-cap-days)
-      period)))
+      (if cap-triggered?
+        (vector (assoc period :marked :remove)
+                (-> period
+                    (assoc :marked :applied)
+                    (assoc :episodes episodes)
+                    (assoc :duration total-duration)
+                    (assoc :end (time/days-after beginning total-duration))))
+        (vector (assoc period :marked :not-applied)))
+      (vector (assoc period :marked :not-applicable)))))
 
 (defn apply-stochastic-rule-to-simulation
-  [periods placement duration-cap-days probability-cap-applies]
+  [periods placement duration-cap-days probability-cap-applies random-seed]
   (into []
-        (map #(apply-stochastic-rule-to-period % placement duration-cap-days probability-cap-applies))
-        periods))
+        (comp (map (fn [[period random-seed]]
+                     (apply-stochastic-rule-to-period period placement duration-cap-days probability-cap-applies random-seed)))
+              cat)
+        (map vector periods (r/split-n random-seed (count periods)))))
 
 (defn apply-duration-scenario-rule
-  [simulations {:keys [placement duration-cap-days probability-cap-applies]}]
-  (into []
-        (map #(apply-stochastic-rule-to-simulation % placement duration-cap-days probability-cap-applies))
-        simulations))
+  [{:keys [simulations random-seed] :as state} {:keys [placement duration-cap-days probability-cap-applies]}]
+  (-> state
+      (update :simulations (partial into [] (map #(apply-stochastic-rule-to-simulation % placement duration-cap-days probability-cap-applies random-seed))))
+      (update :random-seed r/next-seed)))
 
 (defn duration-scenario!
-  [{:keys [input-periods output-periods output-periods-csv scenario-parameters project-to] :as config}]
+  [{:keys [input-periods output-periods output-periods-csv scenario-parameters project-to random-seed] :as config}]
   (let [period-simulations (read/periods input-periods)
         parameters (read/scenario-parameters scenario-parameters)
-        scenario-periods (reduce apply-duration-scenario-rule period-simulations parameters)]
+        scenario-periods (reduce apply-duration-scenario-rule {:simulations period-simulations
+                                                               :random-seed (r/seed random-seed)}
+                                 parameters)]
     (write/write-edn! output-periods scenario-periods)
-    (->> (write/episodes-table project-to scenario-periods)
+    (->> scenario-periods
+         (remove (comp #{:remove} :marked))
+         (write/episodes-table project-to)
          (write/write-csv! output-periods-csv))))
